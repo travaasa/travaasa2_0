d377087679efd654bac2ea736a292af39d814cd7
mikekrueger <mkrueger@doner.com>
2013-04-10 16:57:47 -0400

    Updated non-location pages to use include files

---
+++ site-map/index.php
@@ -0,0 +1,78 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa - Real Travel for Real Experiences.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_DEFAULT ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu.php");
+?><section id="feature1_wrapper" class="subpage_bg"><img src="/assets/images/bg_hana_clouds.jpg" alt=""></section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+  		<h1>Site Map</h1>
+  </section>
+  		<section class="pad grey group">
+        	<div class="col span_1_of_3">
+            	<h5><a href="/austin">Travaasa Austin</a></h5>
+              	<p><a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC" target="_blank" onclick="_gaq.push(['_link', 'https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC']); return false;">MAKE A RESERVATION</a><br>
+                <a href="/austin/austinphotos">GALLERY</a><br>
+              	<a href="/austin/austin-experiences">EXPERIENCES</a><br>
+              	<a href="/austin/travaasa-spa-overview">SPA</a><br>
+              	<a href="/austin/accomodations">ROOMS</a><br>
+              	<a href="/austin/dining">DINING</a><br>
+              	<a href="/austin/groups-and-meetings">GROUPS &amp; MEETINGS</a></p>
+            </div>
+        	<div class="col span_1_of_3">
+            	<h5><a href="/hana">Travaasa Hana</a></h5>
+              	<p><a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=HNMHM&amp;numberOfAdults=2" target="_blank" onclick="_gaq.push(['_link', 'https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=HNMHM&amp;numberOfAdults=2']); return false;">MAKE A RESERVATION</a><br>
+                <a href="/hana/hanaphotos">GALLERY</a><br>
+                  <a href="/hana/travaasa-hana-experiences">EXPERIENCES</a><br>
+                  <a href="/hana/hana-the-spa-at-travaasa-hana">SPA</a><br>
+                  <a href="/hana/accomodations">ROOMS</a><br>
+                  <a href="/hana/hana-dining-overview">DINING</a><br>
+                  <a href="/hana/hana-rates-and-packages">RATES</a><br>
+                  <a href="/hana/hana-maui">HANA &amp; GETTING HERE</a><br>
+                  <a href="/hana/hana-groups-and-meetings">GROUPS &amp; MEETINGS</a><br>
+                  <a href="/hana/hana-off-property-excursions">OFF-PROPERTY EXCURSIONS</a></p>
+            </div>
+            <div class="col span_1_of_3">
+      			<h5><a href="/">Travaasa</a></h5>
+                <p><a href="/about-us">ABOUT TRAVAASA</a><br>
+                <a href="/contact-us">CONTACT US</a><br>
+                <a href="http://www.travaasa.com/press-room/">PRESS ROOM</a><br>
+                <a href="http://www.travaasa.com/blog">BLOG</a><br>
+                <a href="http://giftcards.travaasa.com/">GIFT CARDS</a><br>
+                <a href="https://theapplicantmanager.com/php/careers.php?co=tv">CAREERS</a><br>
+                
+                <a href="http://www.facebook.com/travaasa" target="_blank">FACEBOOK</a><br>
+                <a href="http://www.twitter.com/travaasa" target="_blank">TWITTER</a><br>
+                <a href="http://www.data2gold.com/gallery/travaasa/eClub/eClub.html">SIGN UP FOR OUR NEWSLETTER</a></p>
+    		</div>
+          </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+});
+</script>
+</body>
+</html>
