aa4639638b23e21c03436f863486d2c65136a498
Tanner Powell <thetannerpowell@gmail.com>
2013-04-14 15:24:42 -0700

    Mothers Day 1st Draft

---
+++ mothersday/index.php
@@ -0,0 +1,73 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa - Real Travel for Real Experiences.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_AUSTIN ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Austin.php");
+?><section id="feature1_wrapper" class="subpage_bg"><img src="/assets/images/bg_hana_clouds.jpg" alt=""></section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+        <br />
+        <h1>Mothers Day Package at Travaasa Austin</h1>
+        <h2>Sunday, May 12th</h2>
+        <p>Celebrate a deserving mother with a special package that includes:</p>
+        <ul>
+          <li>50min Custom Massage</li>
+          <li>4-course Dinner</li>
+          <li>Access to Travaasa's regularly-scheduled on-property activities</li>
+        </ul>
+        <h4>Starting price of $280</h4>
+        <p>For <strong>$45 additional</strong>, mom will enjoy paired wines with dinner, hand selected by Travaasa’s sommelier. Husbands and other family members can join mom for dinner for a fee of $65 per person.</p>
+        <p>Activities mom can enjoy:</p>
+        <ul>
+          <li>7:45am - Morning Stretch</li>
+          <li>8:00am - Tai Chi and Nature</li>
+          <li>8:30am - Heart Starter Hike</li>
+          <li>8:30am - Nurturing Meditation</li>
+          <li>9:00am - Foam Roller</li>
+          <li>10:00am - Challenge Course</li>
+          <li>10:00am - Pre/Post Natal Yoga</li>
+          <li>10:30am - Equine Encounter</li>
+          <li>11:00am - Mom, Shall We Dance</li>
+          <li>11:30am - Roping</li>
+          <li>12:00pm - Embrace Quiet, Yin Yoga</li>
+          <li>1:00pm - Couples Connection</li>
+          <li>1:00pm - Embrace Journaling</li>
+          <li>1:00pm - GeoCaching</li>
+          <li>1:30pm - T1 Racing</li>
+          <li>2:00pm - Cultivate Happiness Collage</li>
+          <li>3:00pm - Slacklining</li>
+        </ul>  
+<div class="col span_1_of_1"><a href="/pdf/Travaasa-Austin-Property-Map.pdf" class="btn fitwidth" target="_blank">Property Map</a></div>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Austin.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	$('a[data-target="flare"]').peFlareLightbox();
+	setLocation("Austin", "rooms");
+});
+</script>
+</body>
+</html>
