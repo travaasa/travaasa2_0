fe7258e6626d851ce4e0cdf3d87b39445c396778
Tanner Powell <thetannerpowell@gmail.com>
2013-04-15 09:31:42 -0700

    Revert "Revert "Hana using includes now""
    This reverts commit 83abb47d0e5d511e753c529126cd280a7b4db892.

---
+++ hana/hana-dining-overview/index.php
@@ -0,0 +1,61 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa - Real Travel for Real Experiences.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_HANA ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Hana.php");
+?><section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <!--<div class="slide"><img data-original="/assets/images/placeholder_food_A1.jpg" src="/assets/images/global/placeholder.gif" alt=""><div class="overlay-content-new"><h2>DISH NAME GOES HERE / <span>SHORT DESCRIPTION HERE PLEFF LOREM IPSUM DOLOR SIT AMET</span></h2></div></div>
+      <div class="slide"><img data-original="/assets/images/placeholder_food_A2.jpg" src="/assets/images/global/placeholder.gif" alt=""><div class="overlay-content-new"><h2>DISH NAME GOES HERE / <span>SHORT DESCRIPTION HERE PLEFF LOREM IPSUM DOLOR SIT AMET</span></h2></div></div>//-->
+      <div class="slide"><img data-original="/assets/images/feature_hana_1600x640_dining.jpg" src="/assets/images/global/placeholder.gif" alt=""></div>
+      <div class="slide"><img data-original="/assets/images/feature_hana_1600x640_dining_fish.jpg" src="/assets/images/global/placeholder.gif" alt=""><div class="overlay-content-new"><h2>Charred Wild New Zealand Salmon / <span>Asian Vegetables, Salad Balsamic Reduction &amp; Micro Greens</span></h2></div></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+      <div class="col span_8_of_9">
+          <h1>DINING AT TRAVAASA HANA</h1>
+          <p>With the incredible abundance and inspiration of Hana to draw from, our Executive Chef has created a dining experience that's pure Travaasa – a celebration of life and the bounty of the earth that nourishes us body and soul. He's coined a name for his eclectic, creative culinary style: Hana Fusion, a combination of techniques from classical to innovative blending bold flavors with a focus on in-season, local ingredients.</p>
+      </div>
+  </section>
+  <section class="grey pad group">
+    <dl class="floatright span_3_of_7 inset">
+    	<dt><img src="/assets/images/hana_dining_executive_chef.jpg" alt="Executive Chef, Barry Villiarimo" class="fitwidth"></dt>
+        <dd></dd>
+    </dl>
+    <p><strong>Executive Chef, Barry Villiarimo</strong><br>Chef Barry Villiarimo returned to Hana in 2010, coming full circle to where his career began. Raised in Hana, Chef Barry spent the previous twenty years heading up some of the finest hotel restaurants on the West Coast, including the Westin Bonaventure Hotel &amp; Suites in Los Angeles.<br><br>With a focus on farm-to-table cuisine, coupled with a blending of indigenous Hawaiian ingredients whenever possible, Chef Barry's attention to healthy eating is the perfect complement to Travaasa Hana's commitment to our guest's culinary enjoyment, health and well being.</p>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Hana.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	createSubpageStorylines(true, false);
+	setLocation("Hana", "dining");
+});
+</script>
+</body>
+</html>
---
+++ hana/video/index.php
@@ -0,0 +1,53 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa - Real Travel for Real Experiences.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_HANA ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Hana.php");
+?><section id="feature1_wrapper" class="feature_carousel clearfix">
+    <div id="feature1" class="feature">
+      	<div class="slide"><a data-target="flare" href="http://www.youtube.com/watch?v=NOtwkBLTFzM" id="lnk"><img data-original="/assets/images/feature_hana_1600x640_dayvideo_promo.jpg" src="/assets/images/global/placeholder.gif" alt=""></a></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix">
+      <div class="col span_6_of_9">
+          <h1>TRAVAASA HANA: MAUI'S LAST TRULY HAWAIIAN PLACE</h1>
+          <p>Long considered one of the most remarkable resorts in Hawaii, Travaasa Hana (formerly known as Hotel Hana Maui) is now even better. In fact, it was voted the "No. 1 Resort in Hawaii" in <em>Condé Nast Traveler</em>'s 2011 Reader's Choice Awards.<br><br>Located in the town of Hana on the eastern tip of Maui, Travaasa Hana is the first resort on the island to greet the rising sun. Set amid rolling grass fields and palm trees above Kaihalulu Bay, Travaasa Hana will immerse you in the real Hawaii.</p>
+      </div>
+      <div class="pricing_grp col span_3_of_9"><?php include_once($path."/Library/Travaasa_Hana_Pricing.php"); ?></div>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Hana.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	createSubpageStorylines(true, true);
+	setLocation("Hana");
+	$("#lnk").click();
+});
+</script>
+</body>
+</html>
