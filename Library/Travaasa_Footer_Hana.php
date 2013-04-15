b25167807c596370ba9c2df9b213e870836a18a3
Tanner Powell <thetannerpowell@gmail.com>
2013-04-15 09:31:41 -0700

    Revert "Revert "Include test""
    This reverts commit 27ffb5466d4416e4f5d8824372d3e14e2e0a30ca.

---
+++ Library/Travaasa_Footer_Hana.php
@@ -0,0 +1,68 @@
+<div id="reservations-bar">
+	<div class="reservations-bar-inner">
+        <p class="phone"><script type="text/javascript">ShowNavisNCPhoneNumber();</script><noscript>1-888-820-1043</noscript></p>
+        <ul class="reservation_nav menu_top">
+            <li class="reservation"><a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=HNMHM&amp;numberOfAdults=2" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=HNMHM&amp;numberOfAdults=2')]); return false;">MAKE A RESERVATION</a></li>
+        </ul>
+        <ul class="pricing">
+            <li>
+                <h2>INCLUSIVE<br><span>Starting at</span></h2>
+                <h3><sup>$</sup>825</h3>
+            </li>
+            <li>
+                <h2>A LA CARTE<br><span>Starting at</span></h2>
+                 <h3><sup>$</sup>375</h3>
+            </li>
+        </ul>
+    </div>
+</div>
+<footer class="clear">
+  <nav id="footer_links" class="group">
+    <div class="col span_1_of_4">
+      <h2 class="hdr">TRAVAASA HANA</h2>
+      <p>5031 Hana Hwy<br>
+		Hana, Hi 96713<br>
+<br>Tel: <script type="text/javascript">ShowNavisNCPhoneNumber();</script><noscript>808-359-2401</noscript><br>
+Fax: 808-248-7202<br>
+<br>
+<a href="http://maps.google.com/maps?hl=en&amp;q=5031+Hana+Highway+Hana,+HI+96713&amp;gs_upl=3125l8296l0l9305l5l5l0l0l0l1l203l724l1.3.1l5l0&amp;bav=on.2,or.r_gc.r_pw.,cf.osb&amp;biw=638&amp;bih=933&amp;um=1&amp;ie=UTF-8&amp;hq=&amp;hnear=0x7954ac25effd1793:0x535647e82e0e776,5031+Hana+Hwy,+Hana,+HI+96713&amp;gl=us&amp;ei=Hh4GT_KFB4nE2wX9zIn5CQ&amp;sa=X&amp;oi=geocode_result&amp;ct=title&amp;resnum=1&amp;ved=0CB0Q8gEwAA5">DIRECTIONS</a></p>
+    </div>
+    <div class="col span_1_of_4">
+      <p><a href="/hana/hanaphotos">GALLERY</a><br>
+      <a href="/hana/travaasa-hana-experiences">EXPERIENCES</a><br>
+      <a href="/hana/hanaschedule">EXPERIENCE SCHEDULE</a><br>
+      <a href="/hana/hana-the-spa-at-travaasa-hana">SPA</a><br>
+      <a href="/hana/accomodations">ROOMS</a><br>
+      <a href="/hana/hana-dining-overview">DINING</a><br>
+      <a href="/hana/hana-rates-and-packages">RATES</a><br>
+      <a href="/hana/hana-maui">HANA &amp; GETTING HERE</a><br>
+      <a href="/hana/hana-groups-and-meetings">GROUPS &amp; MEETINGS</a><br>
+      <a href="/hana/hana-off-property-excursions">OFF-PROPERTY EXCURSIONS</a></p>
+    </div>
+    <div class="col span_1_of_4">
+      <h2 class="hdr">INQUIRIES</h2>
+      <p><a href="/about-us">ABOUT TRAVAASA</a><br>
+      <a href="/contact-us">CONTACT US</a><br>
+      <a href="http://www.travaasa.com/press-room/">PRESS ROOM</a><br>
+      <a href="http://www.travaasa.com/blog">BLOG</a><br>
+      <a href="http://giftcards.travaasa.com/">GIFT CARDS</a><br>
+      <a href="https://theapplicantmanager.com/php/careers.php?co=tv">CAREERS</a><br>
+	  <a href="/site-map">SITE MAP</a></p>
+    </div>
+    <div class="col span_1_of_4">
+      <h2 class="hdr">FOLLOW US</h2>
+      <ul id="social" class="clearfix">
+      	<li><a href="http://www.facebook.com/travaasa" target="_blank" id="facebook"><img src="/assets/images/global/transparent.gif" alt="Facebook"></a></li>
+        <li><a href="http://www.twitter.com/travaasa" target="_blank" id="twitter"><img src="/assets/images/global/transparent.gif" alt="Twitter"></a></li>
+        <li><a href="http://www.youtube.com/user/Travaasa" target="_blank" id="youtube"><img src="/assets/images/global/transparent.gif" alt="YouTube"></a></li>
+        <li><a href="http://pinterest.com/travaasa/" target="_blank" id="pinterest"><img src="/assets/images/global/transparent.gif" alt="Pinterest"></a></li>
+        <li><a href="http://travaasa.com/blog" target="_blank" id="blog"><img src="/assets/images/global/transparent.gif" alt="Blog"></a></li>
+      </ul>
+      <p><a class="btn" href="http://www.data2gold.com/gallery/travaasa/eClub/eClub.html">SIGN UP FOR OUR NEWSLETTER</a></p>
+    </div>
+<p class="clear" id="footer-copyright"><a title="Preferred Boutique" target="_blank" href="http://preferredhotelgroup.com/preferred-boutique/destinations/hana/travaasa-hana/"><img src="/assets/images/global/preferred-logo-white-144px.png" alt=""></a><span>&copy;2013 Green Tea, LLC.</span></p>
+  </nav>
+</footer>
+<script type="text/javascript">
+var loc = "hana";
+</script>
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa - Real Travel for Real Experiences.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=8558687282"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu.php");
+?>
+<section id="feature1_wrapper" class="feature_carousel clearfix">
+    <div id="feature1" class="feature">
+      <div class="slide"><a href="/hana/"><img src="/assets/images/feature_home_1600x640_adventure.jpg" alt=""></a></div>
+      <div class="slide"><a href="/austin/dining/"><img data-original="/assets/images/feature_home_1600x640_culinary.jpg" src="/assets/images/global/placeholder.gif" alt=""></a></div>
+      <div class="slide"><a href="/hana/"><img data-original="/assets/images/feature_home_1600x640_culture.jpg" src="/assets/images/global/placeholder.gif" alt=""></a></div>
+      <div class="slide"><a href="/austin/"><img data-original="/assets/images/feature_home_1600x640_fitness.jpg" src="/assets/images/global/placeholder.gif" alt=""></a></div>
+      <div class="slide"><a href="/austin/travaasa-spa-overview/"><img data-original="/assets/images/feature_home_1600x640_spa.jpg" src="/assets/images/global/placeholder.gif" alt=""></a></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a> <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
+<section id="content" class="clearfix">
+<article id="main" class="clearfix">
+  <section class="section group" class="clearfix">
+  	<div class="pad">
+        <div class="col span_4_of_7">
+            <div id="location-selector">
+                <h1>SELECT A LOCATION</h1>
+                <ul>
+                    <li><a href="./hana">HANA, MAUI</a></li>
+                    <li><a href="./austin">AUSTIN, TX</a></li>
+                </ul>
+             </div>
+         </div>
+        <div class="col span_3_of_7 col_reservation">
+            <a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC')]); return false;" class="btn">BOOK AUSTIN</a>&nbsp;
+            <a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=HNMHM&amp;numberOfAdults=2" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=HNMHM&amp;numberOfAdults=2')]); return false;" class="btn">BOOK HANA</a>
+         </div> 
+     </div>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?>
+<script type="text/javascript">
+$(document).ready(function(e) {
+	createSubpageStorylines(true, false);
+});
+</script>
+</body>
+</html>
