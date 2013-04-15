fd84299369be4a85457aa0a43462879e4dce5a56
Tanner Powell <thetannerpowell@gmail.com>
2013-04-15 09:31:42 -0700

    Revert "Revert "Nails, Teambuilding content + elopement & Kamaaina pages""
    This reverts commit bb98c225a7b945f3cebdf748de669837ad0ae201.

---
+++ elopement/index.php
@@ -0,0 +1,66 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa - Real Travel for Real Experiences.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_HANA ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Hana.php");
+?><section id="feature1_wrapper" class="subpage_bg"><img src="/assets/images/bg_hana_clouds.jpg" alt=""></section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+          <h1>Weddings in Paradise</h1>
+          <p>Nestled between the Haleakala mountain range and the Pacific Ocean, the tropical oasis of Hana on the east coast of Maui awaits. In ancient Hawaiian legend from Hana, Maui was pulled out of the ocean and born into the Hawaiian island chain. Your new life together begins in the exact same spot. Surrounded by mystery and beauty, historic Hana town exudes a blend of aloha spirit and small town charm.</p>
+          <p>The journey to Hana begins the second your plane lands. Your senses are overtaken by the sweet scent of tuberose, pikake and plumeria flowers. As the singing of little birds welcome you and the warm Pacific trade winds envelop your being, rejoice as you step into the land of Aloha.</p>
+          <p>Our well-trained and dedicated staff invites you to stay where authentic Hawaiian traditions are a way of life. Try throw net fishing, learn how to dance hula, braid a fresh ti-leaf lei, or play the ukulele. Then pamper yourself in our award-winning spa, go for a bike ride, hike to the 400-foot Waimoku Falls, relax at our infinity pool, frolick at the world-famous Hamoa Beach, or just lay in a hammock and read a book.</p> 
+          <p>An intimate elopement on black lava rock at the Pacific’s edge awaits you. Your vision of an island wedding is a dream come true at our sea ranch resort. Dream away…</p>
+          <h2>Elopement Wedding $5,000</h2>
+          <ul>
+            <li>Two night stay in an ocean view Sea Ranch Cottage with private hot tub</li>
+            <li>Daily breakfast, lunch and dinner for two</li>
+            <li>Choice of one premium activity (such as coastal horseback ride) for two</li>
+            <li>Daily spa treatments for two (four treatments total)</li>
+            <li>Bottle of champagne upon arrival</li>
+            <li>Personal services of Travaasa Hana wedding specialist</li>
+            <li>Arrangement of marriage license or civil union appointment</li>
+            <li>Non-denominational officiant to preside over ceremony</li>
+            <li>Traditional wedding lei for bride and groom, bride and bride, or groom and groom</li>
+            <li>500 count fresh flower scatter at ceremony site</li>
+            <li>Special wedding gift from Travaasa Hana</li>
+            <li>Steaming of couple’s wedding attire</li>
+            <li>Private transportation to and from the ceremony site</li>
+            <li>Witness if needed</li>
+            <li>Fresh flowers scattered in room upon return from the ceremony</li>
+          </ul>
+          <p>Tax & service charge additional.</p>
+  </section>
+  
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Hana.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	$('a[data-target="flare"]').peFlareLightbox();
+	setLocation("Hana", "rooms");
+});
+</script>
+</body>
+</html>
