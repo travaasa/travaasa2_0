8cabaf1f5929f78bdf8e396e8d9cc6d7e60ffaaf
Tanner Powell <thetannerpowell@gmail.com>
2013-04-26 14:02:48 -0500

    recipe contest edits

--- recipecontest/index.php
+++ recipecontest/index.php
@@ -25,18 +25,18 @@
 <article id="main">
   <section class="intro pad clearfix notheater">
         <br />
-        <h1>Mom's Favorite Recipe Contest</h1>
-        <p>Some of the best recipes are handed down generation after generation and become traditions with the family. Celebrate this Mother’s Day with a favorite recipe of your mom’s and win a mother/daughter getaway to Travaasa Austin.</p>
-        <h3>Share mom's favorite recipe for your chance to win</h3>
+        <h2>Mom's Favorite Recipe Contest</h2>
+        <p>Some of the best recipes are handed down generation after generation and become traditions with the family.<br /> Celebrate this Mother’s Day with a favorite recipe of your mom’s and win a mother/daughter getaway to Travaasa Austin.</p>
+        <h4>Share mom's favorite recipe for your chance to win</h4>
         <ul>
           <li>Winner chosen by Travaasa Austin’s Executive Chef Benjamin Baker</li>
-          <li>One grand prize winner of a Total Travaasa two night getaway for two (2) at Travaasa Austin</li>
+          <li>One grand prize winner of a Total Travaasa two-night getaway for two at Travaasa Austin</li>
           <li>Winning recipe will be featured in Travaasa's next newsletter and social channels</li>
         </ul>
+        <iframe height="707" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none"  src="http://travaasa.wufoo.com/embed/k7x1a3/"><a href="http://travaasa.wufoo.com/forms/k7x1a3/">Fill out my Wufoo form!</a></iframe>
+        <p>Having trouble? <a href="http://travaasa.wufoo.com/forms/k7x1a3" target="_blank">Fill out the form on its own page.</a></p>
         <h3>Terms &amp; Conditions</h3>
-        <p>INSERT WUFOO ENTRY FORM</p>
-        <p>INSERT TERMS AND CONDITIONS</p>
-<div class="col span_1_of_1"><a href="/pdf/Travaasa-Austin-Property-Map.pdf" class="btn fitwidth" target="_blank">Property Map</a></div>
+<!--<div class="col span_1_of_1"><a href="/pdf/Travaasa-Austin-Property-Map.pdf" class="btn fitwidth" target="_blank">Property Map</a></div>-->
   </section>
 </article>
 </section>
