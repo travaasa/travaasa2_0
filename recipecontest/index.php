fcdbe10e2a6594d041e21557394056467d6c447b
Tanner Powell <thetannerpowell@gmail.com>
2013-04-30 21:53:58 -0500

    Recipecontest big push

--- recipecontest/index.php
+++ recipecontest/index.php
@@ -20,13 +20,20 @@
 <body>
 <?php 
    include_once($path."/Library/Travaasa_Menu_Austin.php");
-?><section id="feature1_wrapper" class="subpage_bg"><img src="/assets/images/bg_hana_clouds.jpg" alt=""></section>
+?><section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <div class="slide"><img data-original="/assets/images/recipecontest_1600x640.jpg" src="/assets/images/global/placeholder.gif" alt=""></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
 <section id="content">
 <article id="main">
   <section class="intro pad clearfix notheater">
-        <br />
-        <h2>Mom's Favorite Recipe Contest</h2>
-        <p>Some of the best recipes are handed down generation after generation and become traditions with the family.<br /> Celebrate this Mother’s Day with a favorite recipe of your mom’s and win a mother/daughter getaway to Travaasa Austin.</p>
+      <div class="col span_8_of_9">
+          <br />
+          <h2>Mom's Favorite Recipe Contest</h2>
+        <p>Some of the best recipes are handed down generation after generation and become traditions with the family. Celebrate this Mother’s Day with a favorite recipe of your mom’s and win a mother/daughter getaway to Travaasa Austin.</p>
         <h4>Share mom's favorite recipe for your chance to win</h4>
         <ul>
           <li>Winner chosen by Travaasa Austin’s Executive Chef Benjamin Baker</li>
@@ -35,8 +42,49 @@
         </ul>
         <iframe height="707" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none"  src="http://travaasa.wufoo.com/embed/k7x1a3/"><a href="http://travaasa.wufoo.com/forms/k7x1a3/">Fill out my Wufoo form!</a></iframe>
         <p>Having trouble? <a href="http://travaasa.wufoo.com/forms/k7x1a3" target="_blank">Fill out the form on its own page.</a></p>
-        <h3>Terms &amp; Conditions</h3>
-<!--<div class="col span_1_of_1"><a href="/pdf/Travaasa-Austin-Property-Map.pdf" class="btn fitwidth" target="_blank">Property Map</a></div>-->
+      </div>
+  </section>
+  <section class="grey pad group">
+    <h3>Terms &amp; Conditions</h3>
+        <p><em>1. Eligibility:</em> Travaasa Mother’s Day Recipe Contest (the “Contest”) is open only to legal residents of the fifty (50) United States and the District of Columbia who are at least eighteen (18) years old. Employees of Green Tea, LLC, Turner PR, Giant Noise, Amstar, Much and House PR, Doner Advertising and any of their parent and affiliate companies as well as the immediate family (spouse, parents, siblings and children) and household members of each such employee are not eligible. The Contest is subject to all applicable federal, state, local laws and regulations and is void where prohibited. </p>
+
+<p><em>2. Sponsor:</em> Sponsor: Travaasa Experiential Resorts 13500 Farm to Market Road 2769 Austin, TX 78726. Administrator: Turner PR 44 Cook St #650 Denver, CO 80206</p>
+
+<p><em>3. Agreement to Official Rules:</em> Participation in this Contest constitutes entrant’s full and unconditional agreement to these Official Rules and Sponsor’s and Administrator’s decisions, which are final and binding in all matters related to this Contest. Winning a prize is contingent upon fulfilling all requirements set forth herein. </p>
+
+<p><em>4. Timing:</em> The Contest begins on May 1st 2013 at 12:00 a.m. Eastern Time (&#8220;ET&#8221;), ends on May 31st 2013 at 11:59 p.m. ET (the &#8220;Contest Period&#8221;). Wufoo entry form time stamps will serve as official time keeping indicators.</p>
+
+<p><em>5. How to Enter:</em> During the entry period, applicants can visit http://travaasa.com/recipecontest and fill out all information, including an original recipe, for a single eligible entry. Travaasa is not responsible for website downtime, form or entry errors.</p>
+
+<p><em>Entry Guidelines:</em><br/>
+* The recipe must be materially original as deemed by administrators
+* Only one entry per person and/or email address
+* Entry must be in English and in text format</p>
+
+<p><em>Content Restrictions:</em>
+* The Entry must not contain material that violates or infringes another’s rights, including but not limited to privacy, publicity or intellectual property rights, or that constitutes copyright infringement;
+* The Entry must not disparage Sponsor, Administrator or any other person or party affiliated with the Contest;
+* The Entry must not contain material that is inappropriate, indecent, obscene, hateful, tortious, defamatory, slanderous or libelous;
+* The Entry must not contain material that promotes bigotry, racism, hatred or harm against any group or individual or promotes discrimination based on race, gender, religion, nationality, disability, sexual orientation or age; and
+* The Entry must not contain material that is unlawful, in violation of or contrary to the laws or regulations in any state where Entry is created.</p>
+
+<p><em>6. Winner Determination:</em> Winners will be determined by private committee.</p>
+
+<p><em>7. Winner Requirements:</em> Potential winners will be notified by email, mail or phone on or around June 5th, 2013. Each required to complete, electronically sign, and submit a Declaration of Compliance which must be received by Administrator within five (5) days of the date notice or attempted notice is sent, in order to claim his/her prize. If a potential winner cannot be contacted, fails to execute and return the Declaration of Compliance within the required time period, does not comply with these Official Rules, or if a prize is returned as undeliverable, the potential winner forfeits the prize. The Sponsor, in its sole discretion, may choose to award the remaining portion of the prize to an alternate of its choosing. Acceptance of any prize shall constitute and signify winner’s agreement and consent that Sponsor and its designees may use the recipe content, creator attribution, and visual representation in its entirety in perpetuity. Winner’s name, city, state, likeness, photo, Entry and/or prize information in connection with the Contest for promotional, advertising or other purposes, worldwide, in any and all media now known or hereafter devised, including the Internet, without limitation and without further payment, notification, permission or other consideration, except where prohibited by law. Without limiting the generality of these Official Rules, winner shall irrevocably grant, transfer, convey and assign to Sponsor the entirety of the rights in and to the Entry and all renewals and extensions of copyright, and the right to secure copyright registrations thereto in perpetuity including, without limitation, the rights to use the Entry for any and all purposes in any and all media whether now known or hereafter developed, on a worldwide basis, in perpetuity. Winner accepts and acknowledges that Sponsor shall not be obligated to use the Entry and that Sponsor in its sole discretion shall have the right to refrain from using the Entry. Sponsor shall not incur any liability whatsoever to the extent Sponsor chooses to refrain from any exploitation of its rights hereunder. Each winner will indemnify Sponsor, Released Parties and any licensee of Sponsor against all claims, damages, liabilities, and expenses (including reasonable counsel fees and legal expenses) arising out of any breach of these terms. Prizes will be fulfilled 2 – 4 weeks after all winner(s) are confirmed at the end of the Contest. </p>
+
+<p><em>8. Prizes:</em> ONE (1) GRAND PRIZE: A certificate for two-night stay on the Your Travaasa Package. Nights must be used consecutively, and is based on availability. The certificate will expire one year from issue, will be transferable, will have no cash value, and must be mentioned at time of booking and presented at time of check-in.</p>
+
+<p><em>9. Release:</em> By receipt of any prize, each winner agrees to release and hold harmless the Sponsor, Turner PR, Giant Noise, Much and House PR, Doner Advertising, Amstar, Doner Advertising, Administrator and their respective parent companies, subsidiaries, affiliates, suppliers, distributors, advertising/promotion agencies, and prize suppliers, and each of their respective parent companies and each such company’s officers, directors, employees and agents (collectively, the “Released Parties”) from and against any claim or cause of action, including, but not limited to, personal injury, death, or damage to or loss of property, arising out of participation in the Contest or receipt or use or misuse of any prize. </p>
+
+<p><em>10. General Conditions:</em> Sponsor reserves the right to cancel, suspend and/or modify the Contest, or any part of it, if any fraud, technical failures or any other factor impairs the integrity or proper functioning of the Contest, as determined by Sponsor in its sole discretion. If terminated Sponsor may, in its sole discretion, determine the winners from among all non-suspect, eligible Entries received up to time of such action using the judging procedure outlined above. Sponsor, in its sole discretion, reserves the right to disqualify any individual it finds to be tampering with the Entry process or the operation of the Contest or to be acting in violation of these Official Rules or any other promotion or in an unsportsmanlike or disruptive manner and void all associated Entries. Any attempt by any person to deliberately undermine the legitimate operation of the Contest may be a violation of criminal and civil law, and, should such an attempt be made, Sponsor reserves the right to seek damages and other remedies (including attorneys’ fees) from any such person to the fullest extent permitted by law. Sponsor’s failure to enforce any term of these Official Rules shall not constitute a waiver of that provision. </p>
+
+<p><em>11. Limitations of Liability:</em> Released Parties are not responsible for: (1) any incorrect or inaccurate information, whether caused by entrant, printing, typographical or other errors or by any of the equipment or programming associated with or utilized in the Contest; (2) technical failures of any kind, including, but not limited to malfunctions, interruptions, or disconnections in phone lines or network hardware or software; (3) unauthorized human intervention in any part of the Entry process or the Contest; (4) printing, typographical, technical, computer, network or human error which may occur in the administration of the Contest, the uploading, the processing or judging of Entries, the announcement of the prizes or in any Contest-related materials; (5) late, lost, undeliverable, damaged or stolen mail; or (6) any injury or damage to persons or property which may be caused, directly or indirectly, in whole or in part, from entrant’s participation in the Contest or receipt or use or misuse of any prize (including any travel/activity related thereto). Released Parties are not responsible for misdirected or undeliverable Entries or for any technical problems, malfunctions of computer systems, servers, providers, hardware/software, lost or unavailable network connections or failed, incomplete, garbled or delayed computer transmission or any combination thereof. Released Parties are not responsible for any unauthorized third-party use of any Entry.</p>
+
+<p><em>12. Disputes:</em> Except where prohibited, entrant agrees that: (1) any and all disputes, claims and causes of action arising out of or connected with this Contest or any prize awarded shall be resolved individually, without resort to any form of class action, and exclusively by the appropriate Texas State Court located in Travis County, Texas (2) any and all claims, judgments and awards shall be limited to actual out-of-pocket costs incurred, including costs associated with entering this Contest, but in no event attorneys’ fees; and (3) under no circumstances will entrant be permitted to obtain awards for, and entrant hereby waives all rights to claim, indirect, punitive, incidental and consequential damages and any other damages, other than for actual out-of-pocket expenses, and any and all rights to have damages multiplied or otherwise increased. All issues and questions concerning the construction, validity, interpretation and enforceability of these Official Rules, or the rights and obligations of the entrant and Sponsor in connection with the Contest, shall be governed by, and construed in accordance with, the laws of the State of Texas without giving effect to any choice of law or conflict of law rules (whether of the State of Texas or any other jurisdiction), which would cause the application of the laws of any jurisdiction other than the State of Texas. </p>
+
+<p><em>13.</em> Entrant&#8217;s Personal Information: Information collected from entrant is subject to Travaasa Experiential Resort’s Privacy Policy http://travaasa.com/privacypolicy. </p>
+
+<p>©2013 Green Tea, LLC. All rights reserved.</p>
   </section>
 </article>
 </section>
@@ -46,7 +94,7 @@
    include_once($path."/Library/Travaasa_Scripts.php");
 ?><script type="text/javascript">
 $(function() {
-	$('a[data-target="flare"]').peFlareLightbox();
+	createSubpageStorylines(true, false);
 	setLocation("Austin");
 });
 </script>
