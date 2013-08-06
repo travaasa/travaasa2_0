adb4d73c8a342648369bd5c7f4af3dd3e0493b2c
Tanner Powell <thetannerpowell@gmail.com>
2013-08-06 15:33:29 -0500

    Food truck page + banner

---
+++ austin/food-truck/index.php
@@ -0,0 +1,72 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa - Real Travel for Real Experiences.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_AUSTIN ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Austin.php");
+?><section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <div class="slide"><img data-original="/assets/images/food-truck-1600x640.jpg" src="/assets/images/global/placeholder.gif" alt=""></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+      <div class="col span_8_of_9">
+          <h3>INTRODUCING THE TRAVAASA AUSTIN FOOD TRUCK</h3>
+          <p>We're taking our fresh, locally grown ingredients from The Farm at Travaasa Austin and bringing them to the city. The menu will have staple items and unique offerings, and will appear around the Austin community at farmer’s markets, events and more. <em>Scroll down to see the food truck schedule. Dates are subject to change.</em></p>
+          <div class="col span_1_of_2">
+          <script id="-locu-widget" type="text/javascript" src="https://widget.locu.com/widget2/locu.widget.v2.0.js?id=539afaa6f27e0b46c5b9&medium=web"></script>
+          </div>
+          <!--<div class="col span_1_of_2">
+          <img src="/assets/images/farm-fruit-logo-533.png" alt="The Farm at Travaasa Austin" width="320">
+          <h1>EXPERIENCE SCHEDULE</h1>
+          <p>This schedule is subject to change.</p>
+        </div>-->
+      <!--<dl class="floatright span_3_of_7 inset">
+      <dt><img src="/assets/images/farm-fruit-logo-533.png" alt="The Farm at Travaasa Austin" class="fitwidth"></dt>
+        <dd></dd>
+    </dl>-->
+      </div>
+  </section>
+  <section class="grey pad group">
+      <p><iframe id="forecast_embed" type="text/html" display="inline" frameborder="0" height="245" width="630" src="http://forecast.io/embed/#lat=30.444271&lon=-97.864709&name=Austin, TX&color=#D7385E&font=Helvetica&units=us"> </iframe></p>
+      <iframe src="https://www.google.com/calendar/embed?title=Travaasa%20Food%20Truck&amp;showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=travaasa.com_o7fd114vgjauu23q1nul8pq7b8%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=America%2FChicago" style=" border:solid 10px #FFFFFF " width="630" height="900" frameborder="0" scrolling="no"></iframe>
+  </section>
+  <!--<section class="grey pad group">
+    <h3>Terms &amp; Conditions</h3>
+
+<p>©2013 Green Tea, LLC. All rights reserved.</p>
+  </section>-->
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Austin.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	createSubpageStorylines(true, false);
+	setLocation("Austin");
+});
+</script>
+</body>
+</html>
