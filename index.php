1f3547c4396d4a85c5ac95cedb030ac89949001a
Tanner Powell <thetannerpowell@gmail.com>
2013-08-11 12:45:19 -0500

    No Resort Fees at Travaasa

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
              </div>
          </div>
         <div class="col span_3_of_7 col_reservation">
-            <h1 style="display:block; color:#666666; font-size:13px; line-height:20px;">TRAVAASA IS RESORT FEE&#8211;FREE</h1>
+            <h1 style="display:block; color:#666666; font-size:13px; line-height:20px;">NO RESORT FEES AT TRAVAASA</h1>
             <a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC')]); return false;" class="btn">BOOK AUSTIN</a>&nbsp;
             <a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=HNMHM&amp;numberOfAdults=2" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=HNMHM&amp;numberOfAdults=2')]); return false;" class="btn">BOOK HANA</a>
          </div> 
