2b3992e4f4423ccc97eb04073bc28101106310ed
kati <klvillav@gmail.com>
2013-10-07 15:52:51 -0500

    added pagse for media and donation requests

---
+++ donations/index.php
@@ -0,0 +1,50 @@
+
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa Experiential Resorts - Rediscover the Magic of Travel.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_AUSTIN ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Austin.php");?>
+<section id="feature1_wrapper" class="subpage_bg"><img src="/assets/images/bg_hana_clouds.jpg" alt=""></section>
+<!--<section id="feature1_wrapper" class="subpage400_bg"><a href="https://travaasa.ticketbud.com/robert-mondavi-wine-dinner" target="_blank"><img data-original="/assets/images/feature_400_austin_1600x400_mondavi-wine-dinner.jpg" src="/assets/images/feature_400_austin_1600x400_mondavi-wine-dinner.jpg" alt=""></section>-->
+
+<!--<section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <div class="slide"><a href="https://travaasabbqbash.ticketbud.com/travaasa-bbq-bash" target="_blank"><img data-original="/assets/images/feature_austin_1600x640_bbq-bash.jpg" src="/assets/images/global/placeholder.gif" alt=""></a></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>-->
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+          <iframe height="2000" allowtransparency="true" frameborder="0" scrolling="no" style="width:666px; border:none; padding: 20px 20px 20px 20px;" src="https://travaasa.wufoo.com/forms/donation-request/"></iframe>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Austin.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+});
+</script>
+</body>
+</html>
