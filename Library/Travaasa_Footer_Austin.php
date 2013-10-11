3dd2e5a3d4b8999069a98d6703a37d9b70ad0f4b
thetannerpowell@gmail.com <thetannerpowell@gmail.com>
2013-10-11 15:56:14 -0500

    new careers link

--- Library/Travaasa_Footer_Austin.php
+++ Library/Travaasa_Footer_Austin.php
@@ -44,7 +44,7 @@ Fax: 512-506-9737<br>
       <a href="http://www.travaasa.com/press-room/">PRESS ROOM</a><br>
       <a href="http://www.travaasa.com/blog">BLOG</a><br>
       <a href="http://giftcards.travaasa.com/">GIFT CARDS</a><br>
-      <a href="https://theapplicantmanager.com/php/careers.php?co=tv">CAREERS</a><br>
+      <a href="http://travaasa.com/careers">CAREERS</a><br>
       <a href="/austin/groups-and-meetings/#/proposal">REQUEST FOR PROPOSAL</a><br>
 	  <a href="/site-map">SITE MAP</a></p>
     </div>
