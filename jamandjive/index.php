14cedab90734cab32b8a24f0f4b0ceb20585f402
kati <klvillav@gmail.com>
2013-11-27 12:57:27 -0600

    jam and jive prelim site

---
+++ jamandjive/index.php
@@ -0,0 +1,56 @@
+
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa Experiential Resorts - Rediscover the Magic of Travel.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_AUSTIN ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Austin.php");?>
+<!-- <section id="feature1_wrapper" class="subpage_bg"><img src="/assets/images/bg_hana_clouds.jpg" alt=""></section>
+<!--<section id="feature1_wrapper" class="subpage400_bg"><a href="https://travaasa.ticketbud.com/robert-mondavi-wine-dinner" target="_blank"><img data-original="/assets/images/feature_400_austin_1600x400_mondavi-wine-dinner.jpg" src="/assets/images/feature_400_austin_1600x400_mondavi-wine-dinner.jpg" alt=""></section>-->
+
+<!--<section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <div class="slide"><a href="https://travaasabbqbash.ticketbud.com/travaasa-bbq-bash" target="_blank"><img data-original="/assets/images/feature_austin_1600x640_bbq-bash.jpg" src="/assets/images/global/placeholder.gif" alt=""></a></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>-->
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+    <div class="group">
+  <div class="col span_3_of_3" style="text-align:center;"><a class="btn-jorah scroll" href="http://www.edibleaustin.com/index.php/events-calendar/1661-featured-events/359-jam-jive-at-travaasa" target="_blank">EVENT DETAILS</a></div>
+  <div class="col span_3_of_3" style="text-align:center;"><a class="btn-list scroll" href="/preferences"target="_blank">GET ON THE LIST</a>
+  <div class="col span_3_of_3" style="text-align:center;"><a href="http://www.edibleaustin.com/index.php/events-calendar/1661-featured-events/359-jam-jive-at-travaasa" target="_blank"><img src="/assets/images/Ad_EdibleAustin_FA_HIRES-final.png"></a></div>
+  <!--<div class="col span_1_of_3"><iframe id="widget-frame" src="https://travaasaharvestdinner.ticketbud.com/houston2013/external" scrolling="yes" frameborder="0" style="height: 500px; width: 460px;"></iframe></div>-->
+
+</div>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Austin.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+});
+</script>
+</body>
+</html>
