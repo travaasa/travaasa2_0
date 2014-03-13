b50f5cadd6123e8e8a2bc453d6d13e48d174f17d
thetannerpowell@gmail.com <thetannerpowell@gmail.com>
2014-03-13 11:47:48 -0500

    launch photo release page /release

---
+++ release/index.php
@@ -0,0 +1,62 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa Experiential Resorts - Rediscover the Magic of Travel.</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_AUSTIN ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MW4X3M"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MW4X3M');</script>
+<!-- End Google Tag Manager -->
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Austin.php");
+?><section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <div class="slide"><img data-original="/assets/images/feature_austin_1600x640_culture_bull.jpg" src="/assets/images/global/placeholder.gif" alt=""><div class="overlay-content-new"><h2>BULL FITNESS / <span>TRAVAASA AUSTIN</span></h2></div></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+      <div class="col span_8_of_9">
+          <h2>Travaasa loves your photo/video!</h2>
+          <p>I hereby grant Travaasa Experientail Resorts, its agents and affiliates the unlimited, worldwide, perpetual, unending rights to use, reproduce, distribute, and convey my image/photograph/video in any format or medium now known or subsequently developed, to modify and edit my image/photograph/video, to combine my image/photograph/video with other images, video, audio, text, and other media, to create derivative works incorporating, including, or based on my image/photograph/video, and to sublicense any or all of the foregoing rights. This grant shall be construed broadly.</p>
+          <p>If you have any questions, please contact marketing@travaasa.com for more information.</p>             
+          <p><a class="btn-list scroll" href="/preferences">GET ON THE LIST</a></p>
+      </div>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Austin.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	createSubpageStorylines(true, false);
+	setLocation("all", "");
+});
+</script>
+</body>
+</html>
