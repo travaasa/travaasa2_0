df8cd32be709a107fe8155d42a2f1ef08b511eda
thetannerpowell@gmail.com <thetannerpowell@gmail.com>
2014-06-16 22:08:24 -0500

    added jquery.address 1.6 and removed
    Removed ‘_trackPageview’ per
    https://github.com/asual/jquery-address/issues/151
    
    Didn’t link 1.6 yet, though. May need to install universal analytics
    first per
    https://github.com/asual/jquery-address/commit/d763e8ba4a8688273c014b3ef
    64324e6fda16747#diff-3d814fc2490dbd17075a3f8ad7bd7f64
    
    http://stackoverflow.com/questions/4311137/jquery-address-google-analyti
    cs

--- Library/Travaasa_Scripts.php
+++ Library/Travaasa_Scripts.php
@@ -50,7 +50,6 @@ _gaq.push(['_setDomainName', '.travaasa.com']);
 _gaq.push(['_setAllowLinker', true]);
 _gaq.push(['_setAllowHash', false]);
 _gaq.push(['_setAllowAnchor', true]);
-_gaq.push(['_trackPageview']);
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
