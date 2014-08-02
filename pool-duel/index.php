bbd193a18556352b7b6b6fe54a24fb5296203df8
thetannerpowell@gmail.com <thetannerpowell@gmail.com>
2014-08-01 19:38:53 -0500

    pool duel

---
+++ pool-duel/index.php
@@ -0,0 +1,63 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa Pool Duel - May the best pool win</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_AUSTIN ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MW4X3M"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MW4X3M');</script>
+<!-- End Google Tag Manager -->
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Austin.php");
+?><section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <div class="slide"><img data-original="/assets/images/1600x640_pool-duel-hero.jpg" src="/assets/images/global/placeholder.gif" alt=""><div class="overlay-content-new"><h2><span>TRAVAASA AUSTIN / TRAVAASA HANA, MAUI</span></h2></div></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+      <div class="col span_8_of_9">
+          <h1>THE TRAVAASA POOL DUEL</h1>
+          <p>Vote for your favorite pool and be entered to win three (3) nights for two at the Travaasa Experiential Resort of your choice + $500 in airfare credits.</p>
+              <div><iframe height="500" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none"  src="https://travaasa.wufoo.com/embed/q18b2kb81dankm2/"></iframe></div>            
+                        <!--<p><a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC&amp;rateCode=TTTXM&amp;numberOfAdults=2" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC&amp;rateCode=TTTXM&amp;numberOfAdults=2')]); return false;" style="margin-bottom:20px;" class="btn-jorah">BOOK TOTAL TRAVAASA</a>&nbsp;&nbsp;&nbsp;
+            <a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC&amp;rateCode=YTTXM&amp;numberOfAdults=2" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC&amp;rateCode=YTTXM&amp;numberOfAdults=2')]); return false;" class="btn-jorah">BOOK YOUR TRAVAASA</a><br><br><a class="btn-list scroll" href="/preferences">GET ON THE LIST</a></p>-->
+      </div>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Austin.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	createSubpageStorylines(true, false);
+	setLocation("all", "");
+});
+</script>
+</body>
+</html>
