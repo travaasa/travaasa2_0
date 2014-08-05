d6a0a52b2c50becca8d55afd4b3997a5a5a5a90d
thetannerpowell@gmail.com <thetannerpowell@gmail.com>
2014-08-05 12:47:11 -0500

    Merge branch 'dev'

---
+++ pool-duel/terms/index.php
@@ -0,0 +1,104 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa Pool Duel - May the best pool win</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_AUSTIN ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MW4X3M"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MW4X3M');</script>
+<!-- End Google Tag Manager -->
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Austin.php");
+?><section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <div class="slide"><img data-original="/assets/images/1600x640_pool-duel-hero.jpg" src="/assets/images/global/placeholder.gif" alt=""><div class="overlay-content-new"><h2><span>TRAVAASA AUSTIN / TRAVAASA HANA, MAUI</span></h2></div></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+      <div class="col span_8_of_9">
+          <h2>Travaasa Experiential Resorts® Pool Duel – TERMS &amp; CONDITIONS</h2>
+
+<h4>NO PURCHASE NECESSARY.</h4>
+
+<p>A PURCHASE WILL NOT AFFECT OR IMPROVE THE CHANCES OF WINNING. Sweepstakes is subject to all applicable laws and regulations. Void where prohibited or restricted by law.</p>
+
+<h4>Sponsor</h4>
+
+<p>Travaasa Experiential Resorts®, 1050 Seventeenth Street, 23rd Floor, Denver, Colorado 80265.</p>
+
+<h4>How to Enter</h4>
+
+<p>Submit your name and email address on the entry page on <strong><a href="http://travaasa.com/pool-duel">http://travaasa.com/pool-duel</a></strong>. Participants can only enter once per email address. The winners of Travaasa Experiential Resorts’ “Pool Duel” Sweepstakes will be selected at random from all of the entries completed in full by September 15, 2014.</p>
+
+<h4>Eligibility</h4>
+
+<p>Sweepstakes is open only to legal residents of the United States who are at least 18 years of age as of the date of entry. Sweepstakes is not open to employees, independent contractors, officers, directors or agents of Sponsor or any of Sponsor’s parents, affiliates, subsidiaries, successors and/or assigns, Sponsor’s advertising, promotion and fulfillment agencies, and legal advisors, and the immediate family members (mother, father, brother, sister, daughter, son or spouse, regardless of where they live) and persons living in the same household (whether or not related) of any of the foregoing (collectively, “Sweepstakes Entities”).</p>
+
+<h4>Sweepstakes Period</h4>
+
+<p>The Submission Period begins on June 5 at 9:00AM PST and ends August 31 2014 at 11:59 PST. Winners will be announced within the time period after August 31 to allow for time to notify and confirm the winner. Sponsor is not responsible for lost, late, incomplete, or illegible entries. Incomplete information may result in disqualification of entry.</p>
+
+<h4>Winner Selection</h4>
+
+<p>One (1) grand prize winner will be selected at random on or around or around August 31, 2014 and will be notified by email that same day.</p>
+
+<h4>Prize</h4>
+
+<p>The Grand Prize includes two (2) $250 Visa Giftcards to be used towards travel expenses to winners choice of Travaasa Austin or Travaasa Hana and a three-night stay with the Total Travaasa Package at either property for two (2), based on double occupancy (one room total). The Total Travaasa Package includes: Two (2) $125 resort credits good towards spa, three meals per day prepared by the Travaasa Culinary Team, all snacks and non-alcoholic beverages, all regularly scheduled on-property programs &amp; activities including yoga, fitness classes, cultural classes, culinary classes and more, spacious, well-appointed accommodations featuring organic cotton linens, full access to Travaasa's extensive resort amenities including the 24-hour fitness center featuring FreeMotion™ fitness equipment and free weights; full-use of the infinity-swimming pool; extensive spa facilities and quiet room. <strong>Guests must be 16 years of age or older to stay at Travaasa Austin.</strong></p>
+
+<p>Prizes are non-refundable and non-transferable, cannot be combined with any other special offer or promotion and must be accepted as awarded. Prize may not be exchanged for cash value or substituted, except that Sponsor reserves the right in its sole discretion to substitute with either the cash value of the applicable prize that would otherwise have been awarded, or a prize or prizes of equal or greater value. Applicable federal, state, or local taxes and any other costs or expenses associated with the prizes are the sole responsibility of the winner. Prizes will be delivered to confirmed winner. If airfare amounts to being more than $500, confirmed winner will be in charge of the difference in price. Travaasa Experiential Resorts is not responsible for travel expenses over $500.</p>
+
+<p>By participating, each entrant agrees to comply with these official rules and that Sponsor is not responsible for any incorrect or inaccurate information, whether caused by any of the equipment or programming associated with or utilized in the Sweepstakes or by any technical or human error that may occur in the processing of submissions in the Sweepstakes. Sponsor assumes no responsibility for any error, omission, interruption, deletion, defect, delay in operation or transmission, communications line failure, theft or destruction or unauthorized access to, or tampering with or hacking of, the website or any technical failure or malfunction.
+IN NO EVENT WILL THE SPONSOR OR ANY CONTEST ENTITIES BE RESPONSIBLE OR LIABLE FOR ANY DAMAGES OR LOSSES OF ANY KIND, INCLUDING DIRECT, INDIRECT, INCIDENTAL, CONSEQUENTIAL OR PUNITIVE DAMAGES ARISING OUT OF THE ENTRANT’S PARTICIPATION IN THE SWEEPSTAKES, ANY PRIZES WON, THE ENTRANT’S ACCESS TO OR USE OF ANY OF THE SPONSOR’S WEBSITES, OR THE ACCESSING, DOWNLOADING AND/OR PRINTING OF ANY MATERIAL AVAILABLE ON SAID SITE. WITHOUT LIMITING THE FOREGOING, EVERYTHING ON THE SPONSOR’S WEBSITES IS PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT. SOME JURISDICTIONS MAY NOT ALLOW THE LIMITATION OR EXCLUSION OF LIABILITY FOR INCIDENTAL OR CONSEQUENTIAL DAMAGES OR EXCLUSIONS OF IMPLIED WARRANTIES SO SOME OF THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO THE ENTRANT.</p>
+
+<p>By accepting a SWEEPSTAKES’s prize, the winner, where permitted by law, grant (and agree to confirm that grant in writing at the Sponsor’s request) to Sponsor the right to use and publish his/her name, address, Sweepstakes Submission, portrait, picture, voice and likeness for advertising, trade and promotional purposes without notification, review or approval, and without additional consideration, worldwide and on the Internet. The winner may be required to complete, sign and return an affidavit of eligibility and/or a liability/publicity release, which must be returned within seven (7) days from the date of notification. Failure to sign and return such documents timely, or if prize or prize notification is returned as non-deliverable, or if winner is found to be ineligible or otherwise not in compliance with these official rules, prize will be forfeited and awarded to an alternate winner in Sponsor’s sole discretion in accordance with these official rules.</p>
+
+<p>BY ACCEPTING A PRIZE, THE WINNER RELEASES AND AGREES TO INDEMNIFY AND HOLD HARMLESS SPONSOR AND ALL SWEEPSTAKES ENTITIES FROM ANY AND ALL LIABILITY FOR ANY INJURY, LOSS OR DAMAGE OF ANY KIND TO PERSONS, INCLUDING DEATH, AND PROPERTY, ARISING FROM OR IN CONNECTION WITH ACCEPTANCE, POSSESSION, USE OR MISUSE OF ANY PRIZE, PARTICIPATION IN THIS SWEEPSTAKES OR ANY SWEEPSTAKES-RELATED ACTIVITY.</p>
+
+<p>If, for any reason, the Sweepstakes is not capable of running as planned because of circumstances including, but not limited to, unauthorized intervention, fraud, technical failures, or any other causes, which, in the Sponsor’s sole judgment, corrupts or affects the administration, security, fairness, integrity or proper conduct of this Sweepstakes, Sponsor reserves the right, in its sole discretion and determination of fairness, to cancel, terminate, modify or suspend the Sweepstakes and to select a winner from entries received prior to such action, in accordance with these official rules. Sponsor may disqualify any entrant who tampers with the entry process, operation of the Sweepstakes, or violates these official rules.</p>
+
+<p>By registering to enter the Sweepstakes, each entrant acknowledges he/she has read, understands and will abide by these official rules. All issues and questions concerning the construction, validity, interpretation and enforceability of these official rules, or the rights and obligations of entrants and/or Sponsor in connection with the Sweepstakes, shall be governed by and construed in accordance with, the laws of the State of Texas, without regard to the conflict of laws principles.</p>
+
+<p>The personal information collected from each entrant in the course of the Sweepstakes will be used by Travaasa for the purposes of administration of the Sweepstakes, Social Media Sharing, Newsletter Distribution and as otherwise permitted in accordance with Travaasa Experiential Resorts® Privacy Policy.</p>           
+                        <!--<p><a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC&amp;rateCode=TTTXM&amp;numberOfAdults=2" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC&amp;rateCode=TTTXM&amp;numberOfAdults=2')]); return false;" style="margin-bottom:20px;" class="btn-jorah">BOOK TOTAL TRAVAASA</a>&nbsp;&nbsp;&nbsp;
+            <a href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC&amp;rateCode=YTTXM&amp;numberOfAdults=2" target="_blank" onclick="_gaq.push(['_link', AppendNavisKeywordToURL('https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=AUSTC&amp;rateCode=YTTXM&amp;numberOfAdults=2')]); return false;" class="btn-jorah">BOOK YOUR TRAVAASA</a><br><br><a class="btn-list scroll" href="/preferences">GET ON THE LIST</a></p>-->
+      </div>
+  </section>
+</article>
+</section>
+<?php 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Austin.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	createSubpageStorylines(true, false);
+	setLocation("all", "");
+});
+</script>
+</body>
+</html>
