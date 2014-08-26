b11124447dfd0f201b167e79cbc5452890265efd
Yordan Kirilov <jordan.kirilov@gmail.com>
2014-08-26 09:42:06 +0300

    Added "road-to-hana/index.php" to be used for interactive map page and road-to-hana.less/css for styling.

---
+++ hana/road-to-hana/index.php
@@ -0,0 +1,51 @@
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Hana Hawaii Vacation | Maui Resorts | Travaasa Hana</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Discover the town of Hana at the tip of Maui and at the end of the Road to Hana. Discover an extraordinary Maui resort vacation experience at Travaasa Hana." />
+	<?php
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_HANA ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MW4X3M"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MW4X3M');</script>
+<!-- End Google Tag Manager -->
+<?php
+   include_once($path."/Library/Travaasa_Menu_Hana.php");
+?>
+
+<section id="content">
+  <article id="main">
+    
+  </article>
+</section>
+<?php
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Hana.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	createSubpageStorylines(true, false);
+	setLocation("Hana", "locationinfo");
+	activateTabbedList();
+});
+</script>
+</body>
+</html>
