b12b69b577131e9023bc61abe30ec54bd4f0fe8f
thetannerpowell@gmail.com <thetannerpowell@gmail.com>
2014-09-30 14:59:12 -0500

    october calendar switch

--- calendar/2014/september/index.php
+++ calendar/2014/september/index.php
@@ -246,9 +246,9 @@ endif;
   <section class="grey pad">
         <h4>See upcoming months</h4>
       <div class="group">
-            <div class="col span_1_of_3 month"><div><a href="/calendar/2014/august/"><img src="/assets/images/months/aug-dive.png" alt="August is Dive Month"></a></div></div>
             <div class="col span_1_of_3 month"><div class="active"><a href="/calendar/2014/september/"><img src="/assets/images/months/sep-discover.png" alt="September is Discover Month"></a></div></div>
             <div class="col span_1_of_3 month"><div><a href="/calendar/2014/october/"><img src="/assets/images/months/oct-harvest.png" alt="October is Harvest Month"></a></div></div>
+            <div class="col span_1_of_3 month"><div><a href="/calendar/2014/november/"><img src="/assets/images/months/nov-stop.png" alt="November is Stop Month"></a></div></div>
         </div>
   </section>
   
