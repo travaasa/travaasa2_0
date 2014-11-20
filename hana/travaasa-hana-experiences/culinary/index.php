41adf4a8b757303394fb3d10662cb54f41c853cd
Chris <hristo.ginev@aggressione.com>
2014-11-20 13:39:08 -0800

    merge

--- hana/travaasa-hana-experiences/culinary/index.php
+++ hana/travaasa-hana-experiences/culinary/index.php
@@ -53,7 +53,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <div>
         <article>
           <img data-original="/assets/images/exp_hana_culinary_healthy_regional_cuisine.jpg" src="/assets/images/exp_hana_culinary_healthy_regional_cuisine.jpg" alt="Healthy Regionl Cuisine Hawaii Culinary travel">
-<<<<<<< HEAD
           <br />
           <h2>Healthy Regional Cuisine</h2>
           <p class="description">You won’t find dishes like those of modern Hawaiian cuisine anywhere else in the world. Here, Hawaii’s traditional foods meet with the many diverse ethnic flavors brought to the islands over a long and rich history of immigration, resulting in a local fusion with tastes of Japan, China and the Philippines, to name a few. The island is also a paradise for gourmets, with abundant fruits and vegetables grown from rich volcanic soil, a wide variety of high quality, fresh fish and locally sourced cattle. During this experience, you'll learn about Hawaii's many food staples and try your hand at some of our most revered local delicacies.</p>
@@ -61,36 +60,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <article>
           <img data-original="/assets/images/exp_hana_culinary_juicing.jpg" src="/assets/images/exp_hana_culinary_juicing.jpg" alt="Juicing 2.0">
           <br />
-=======
-          <h2>Healthy Regional Cuisine</h2>
-          <p class="description">You won’t find flavors like those of modern Hawaiian cuisine anywhere else in the world. Here, Hawaii’s traditional foods meet with the many diverse ethnic flavors brought to the islands over a long and rich history of immigration, resulting in a local fusion with tastes of Japan, China and the Philippines, to name just a few. The island is also a paradise for gourmets, with abundant fruits and vegetables grown from rich volcanic soil, a wide variety of high quality, fresh fish and locally sourced cattle. During this experience, you'll learn about Hawaii's many food staples and try your hand at some of our most revered local dishes.</p>
-        </article>
-        <article>
-          <img data-original="/assets/images/exp_hana_culinary_juicing.jpg" src="/assets/images/exp_hana_culinary_juicing.jpg" alt="Juicing 2.0">
->>>>>>> c56a783599de4f587d715ea68d68648e65ca8e50
           <h2>Juicing 2.0</h2>
           <p class="description">From boosting your immune and digestive systems to aiding in weight loss and detoxifying your body, there are many health benefits to juicing, and if there was ever an ideal place for you to try juicing, it's Hawaii. The rich and varied bounty of fruits and vegetables lets you whip up a nearly endless list of juicy creations. Travaasa Hana will guide you through this interactive experience so you get the most benefits out of your juice. But this isn't “old school” blending filled with chopping, slopping and mopping here. This is Juicing 2.0 Hana style. Together, we'll use whole foods to blend and mix fresh juices, soups, dips, spreads, desserts and salsas.</p>
         </article>
         <article>
           <img data-original="/assets/images/exp_hana_culinary_art_cocktail.jpg" src="/assets/images/exp_hana_culinary_art_cocktail.jpg" alt="The Art of the Cocktail: Cocktail mixology">
-<<<<<<< HEAD
           <br />
           <h2>The Art of the Cocktail</h2>
           <p class="description">We are in a mixology renaissance. More and more lounges, bars and restaurants are offering lists of aperitifs, specialty cocktails and after-dinner drinks that sparkle with sophistication and creativity. So the bartender's trick is how best to combine juices, purees, fruits, spices and wonderful fresh ingredients to develop seductive new offerings and reinvigorate the classics we all know and love — and there’s no better place to play with fresh fruit flavors or spices than on the island of Maui. With endless combinations of fresh ingredients at our fingertips, this in-depth exploration will deepen your appreciation for the art of the cocktail.
-=======
-          <h2>The Art of the Cocktail</h2>
-          <p class="description">We are in a mixology renaissance. More and more lounges, bars and restaurants are offering lists of aperitifs, specialty cocktails and after-dinner drinks that sparkle with sophistication and creativity. So the bartender's trick is how best to combine juices, purees, fruits, spices and wonderful fresh ingredients to develop seductive new offerings and reinvigorate the classics we all know and love — and there’s no better place to play with fresh fruit flavors or spices than on the island of Maui. With endless combinations of fresh ingredients at our fingertips, this in-depth exploration will deepen your appreciation for the art of the cocktail. 
->>>>>>> c56a783599de4f587d715ea68d68648e65ca8e50
           <br><br>
           Find out why the Mai Tai is synonymous with Hawaii — or is it? Learn the difference between a martini that is shaken or stirred. Discover what a muddle is and why you would use it. Join us as we embark on a tasting journey, learning how quintessential old-world recipes have morphed in search of the complex, layered flavors that create today's savory new trends.
           </p>
         </article>
         <article>
           <img data-original="/assets/images/exp_hana_culinary_tasting_series.jpg" src="/assets/images/exp_hana_culinary_tasting_series.jpg" alt="Coconut Husking">
-<<<<<<< HEAD
           <br />
-=======
->>>>>>> c56a783599de4f587d715ea68d68648e65ca8e50
           <h2>Coconut Husking</h2>
           <p class="description">Found across most of the tropics, the coconut, or niu, is known for its great versatility as seen in the many domestic, commercial, and industrial uses of its different parts. Recently, the health benefits of coconut water have made it a staple in health food stores around the country, while coconut oil is making a name for itself as both a cooking oil and a health and beauty product. Since ancient times, coconuts have been part of the daily diet of Hawaiian people. Witness the skill and tools involved in husking a coconut in traditional Hawaiian fashion, then enjoy fresh coconut water and meat as it's prepared.</p>
         </article>
