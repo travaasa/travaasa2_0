6f1249f7a202a639ee06a44a594e62138e683f28
Tanner Powell <tanner@Tanners-MacBook-Pro-2.local>
2016-02-02 14:18:41 -0600

    hana weddings draft

---
+++ hana/weddings/index.php
@@ -0,0 +1,99 @@
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Maui Wedding Location | Travaasa Hana, Maui | Ceremony and Reception</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
+	<meta name="description" content="Travaasa Hana's Executive Chef has created a dining experience that's pure Hana - authentic Hawaiian dining blending bold flavors with a focus on in-season, local ingredients." />
+	<?php
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_HANA ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MW4X3M"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MW4X3M');</script>
+<!-- End Google Tag Manager -->
+<?php
+   include_once($path."/Library/Travaasa_Menu_Hana.php");
+?><section id="feature1_wrapper" class="feature_carousel sub_feature clearfix">
+    <div id="feature1" class="feature">
+      <!--<div class="slide"><img data-original="/assets/images/placeholder_food_A1.jpg" src="/assets/images/global/placeholder.gif" alt=""><div class="overlay-content-new"><h2>DISH NAME GOES HERE / <span>SHORT DESCRIPTION HERE PLEFF LOREM IPSUM DOLOR SIT AMET</span></h2></div></div>
+      <div class="slide"><img data-original="/assets/images/placeholder_food_A2.jpg" src="/assets/images/global/placeholder.gif" alt=""><div class="overlay-content-new"><h2>DISH NAME GOES HERE / <span>SHORT DESCRIPTION HERE PLEFF LOREM IPSUM DOLOR SIT AMET</span></h2></div></div>//-->
+      <!--<div class="slide"><img data-original="/assets/images/feature_hana_1600x640_dining2.jpg" src="/assets/images/global/placeholder.gif" alt="The Preserve Kitchen + Bar at Travaasa Hana, Dining in Hana, Authentic Hawaiian Food"></div>-->
+      <div class="slide"><img data-original="http://static.travaasa.com/assets/images/feature_hana_weddings_1600x640_01_bluff.jpg" src="/assets/images/global/placeholder.gif" alt="The Ocean Bluff"><div class="overlay-content-new"><h2>The Ocean Bluff / <span>Ceremony</span></h2></div></div>
+      <div class="slide"><img data-original="http://static.travaasa.com/assets/images/feature_hana_weddings_1600x640_03_reception.jpg" src="/assets/images/global/placeholder.gif" alt="Reception at The Preserve"><div class="overlay-content-new"><h2>The Preserve Kitchen + Bar / <span>Reception</span></h2></div></div>
+      <div class="slide"><img data-original="http://static.travaasa.com/assets/images/feature_hana_weddings_1600x640_04_tent.jpg" src="/assets/images/global/placeholder.gif" alt="Plantation Guest House Reception"><div class="overlay-content-new"><h2>Plantation Guest House / <span>Reception</span></h2></div></div>
+    </div>
+    <a class="carousel_prev" id="feature1_prev" href="#"><span><b>prev</b></span></a>
+    <a class="carousel_next" id="feature1_next" href="#"><span><b>next</b></span></a>
+</section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+      <div class="col span_8_of_9">
+          <h1>WEDDINGS AT TRAVAASA HANA, MAUI</h1>
+          <p>Let the lush beauty of Maui's landscape inspire your tropical dream wedding. Whether you envision an intimate ocean bluff ceremony or a grand Lu’au lasting late into the night, Travaasa Hāna provides the perfect scene for your life-changing union. <br /><br />
+          Four outdoor ceremony locations and eight indoor or outdoor reception locations offer the perfect venue for weddings of any size. Customize your dream wedding with add ons such as a tropical flower crown, hula dancers, a ukulele musician and other options.<br /><br />
+          What better way to relax and prepare for the big day than at our <a href="/hana/hana-the-spa-at-travaasa-hana/" target="_self">award-winning spa</a> with your bridal party?<br /><br />
+          Heavenly Hāna is the perfect location for your Island wedding dreams to come true.</p>
+
+      </div>
+  </section>
+  <section class="grey pad">
+    <h4 style="text-align:center;">GALLERY</h4>
+    <div id="hana-wedding-gallery-wrapper" class="gallery section">
+        <div class="group">
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-001.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-001.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-002.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-002.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-003.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-003.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-005.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-005.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-006.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-006.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+        </div>
+
+        <div class="group">
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-007.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-007.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-008.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-008.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-009.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-009.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-010.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-010.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+            <div class="col span_1_of_5"><a  href="http://static.travaasa.com/assets/images/gallery/hana/1440-hana-weddings-011.jpg" data-flare-gallery="gallery1" data-target="flare" data-flare-scale="fitmax" title="XXXXXXXXXX"><img class="lazy" data-original="http://static.travaasa.com/assets/images/gallery/hana/100-hana-weddings-011.jpg" src="http://static.travaasa.com/assets/images/global/placeholder.gif" width="180" height="180" alt="XXXXXXXXXX"></a></div>
+        </div>
+        <a name="hana-wedding-gallery">&nbsp;</a>
+      </div>
+</section>
+  <!--<section class="grey pad group">
+    <dl class="floatright span_3_of_7 inset">
+    	<dt><img src="/assets/images/hana_dining_executive_chef.jpg" alt="Executive Chef, Barry Villiarimo" class="fitwidth"></dt>
+        <dd></dd>
+    </dl>
+    <p><strong>Executive Chef, Barry Villiarimo</strong><br>Chef Barry Villiarimo returned to Hana in 2010, coming full circle to where his career began. Raised in Hana, Chef Barry spent the previous twenty years heading up some of the finest hotel restaurants on the West Coast, including the Westin Bonaventure Hotel &amp; Suites in Los Angeles.<br><br>With a focus on farm-to-table cuisine, coupled with a blending of indigenous Hawaiian ingredients whenever possible, Chef Barry's attention to healthy eating is the perfect complement to Travaasa Hana's commitment to our guest's culinary enjoyment, health and well being.</p>
+  </section>-->
+</article>
+</section>
+<?php
+
+
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Hana.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	createSubpageStorylines(true, false);
+	setLocation("Hana", "weddings");
+});
+</script>
+</body>
+</html>
