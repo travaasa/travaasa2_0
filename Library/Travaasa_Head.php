dfca77523a78df868f39d969b37a343355e55fe2
Tanner Powell <thetannerpowell@gmail.com>
2017-12-04 10:21:45 -0600

    Revert to jQuery 1.7.2

--- Library/Travaasa_Head.php
+++ Library/Travaasa_Head.php
@@ -25,7 +25,6 @@
 <link rel="stylesheet" href="https://ssl-travaasa.scdn4.secure.raxcdn.com/assets/css/homepage.css" media="all">
 <link rel="stylesheet" href="https://ssl-travaasa.scdn4.secure.raxcdn.com/assets/scripts/pe.flare/jquery.pixelentity.flare.min.css" media="all">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css" media="all">
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.8.0/slick.css"/>
 <!-- Facebook Pixel Code -->
 <script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '365559253633553');fbq('track', "PageView");</script><noscript><img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=365559253633553&ev=PageView&noscript=1"/></noscript>
 <!-- End Facebook Pixel Code -->
