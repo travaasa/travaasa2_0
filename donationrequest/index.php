9b22e9289614ffe567e4fe4eca2e59ea6f92f44c
Tanner Powell <thetannerpowell@gmail.com>
2018-02-16 14:56:42 -1000

    donation request edits

--- donationrequest/index.php
+++ donationrequest/index.php
@@ -1,35 +1,23 @@
-
-<?php 
+<?php
    $path = $_SERVER['DOCUMENT_ROOT'];
    include_once($path."/Library/Travaasa_Common.php");
 ?>
 <!DOCTYPE html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Travaasa Experiential Resorts - Rediscover the Magic of Travel.</title>
-	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, austin, texas, Austin resort, Austin spa, travaasa Austin, Austin spa resort, experiential, hotel, Austin hotel, Austin conference center, Austin group meeting" />
-	<meta name="description" content="Rediscover the magic of travel with Travaasa Austin, Texas or Hana, Maui." />
-	<?php 
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="robots" content="noindex" />
+  <meta name="googlebot" content="noindex" />
+  <title>Donation Request | Travaasa Hana, Maui</title>
+  <meta name="keywords" content="travaasa, travel, resorts, hana, hana, hawaii, experiential, hotel, hotel hana-maui" />
+  <meta name="description" content="Discover the town of Hana at the tip of Maui and at the end of the Road to Hana. Discover an extraordinary Maui resort vacation experience at Travaasa Hana." />
+  <?php
    include_once($path."/Library/Travaasa_Head.php");
-	?>
-<!-- start NAVIS Code -->
-<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_AUSTIN ?>"></script>
-<script type="text/javascript">ProcessNavisNCKeyword();</script>
-<!-- end NAVIS Code -->
+  ?>
 </head>
 <body>
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MW4X3M"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MW4X3M');</script>
-<!-- End Google Tag Manager -->
 <?php 
-   include_once($path."/Library/Travaasa_Menu_Austin.php");?>
+   include_once($path."/Library/Travaasa_Menu_Hana.php");?>
 <section id="feature1_wrapper" class="subpage_bg"><img src="/assets/images/bg_hana_clouds.jpg" alt=""></section>
 <!--<section id="feature1_wrapper" class="subpage400_bg"><a href="https://travaasa.ticketbud.com/robert-mondavi-wine-dinner" target="_blank"><img data-original="/assets/images/feature_400_austin_1600x400_mondavi-wine-dinner.jpg" src="/assets/images/feature_400_austin_1600x400_mondavi-wine-dinner.jpg" alt=""></section>-->
 
@@ -49,12 +37,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 </section>
 <?php
 
- 
+
    include_once($path."/Library/Travaasa_Testimonials.php");
-   include_once($path."/Library/Travaasa_Footer_Austin.php");
+   include_once($path."/Library/Travaasa_Footer_Hana.php");
    include_once($path."/Library/Travaasa_Scripts.php");
 ?><script type="text/javascript">
 $(function() {
+  createSubpageStorylines(true, false);
+  setLocation("Hana", "locationinfo");
+  activateTabbedList();
 });
 </script>
 </body>
