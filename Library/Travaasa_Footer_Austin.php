18e1d1e68302852e2fa32e2b936727c8ac3b2461
Tanner Powell <thetannerpowell@gmail.com>
2019-01-09 07:12:34 -1000

    gallery update & 2019

--- Library/Travaasa_Footer_Austin.php
+++ Library/Travaasa_Footer_Austin.php
@@ -72,7 +72,7 @@ Group Sales: 512-334-4649
         <script type="text/javascript">SetNavisP2ChatCustomLink("lnkP2Chat");</script>-->
       </p>
     </div>
-<p class="clear" id="footer-copyright"><!--<a title="Preferred Boutique" target="_blank" href="https://preferredhotels.com/destinations/austin-tx/travaasa-austin"><img src="https://ssl-travaasa.scdn4.secure.raxcdn.com/assets/images/global/preferred-logo2-white-144px.png" width="144" alt=""></a>--><span>&copy;2018 Green Tea, LLC.&nbsp;&nbsp;&#8226;&nbsp;&nbsp;Travaasa Austin is a non-smoking property.</span></p>
+<p class="clear" id="footer-copyright"><!--<a title="Preferred Boutique" target="_blank" href="https://preferredhotels.com/destinations/austin-tx/travaasa-austin"><img src="https://ssl-travaasa.scdn4.secure.raxcdn.com/assets/images/global/preferred-logo2-white-144px.png" width="144" alt=""></a>--><span>&copy;2019 Green Tea, LLC.&nbsp;&nbsp;&#8226;&nbsp;&nbsp;Travaasa Austin is a non-smoking property.</span></p>
     
     
   </nav>
