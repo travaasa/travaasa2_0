76961c65e0013a9ef8a3758f56d713f22e6efdfc
Tanner Powell <thetannerpowell@gmail.com>
2019-03-08 10:16:34 -1000

    Create ADA Amenities

---
+++ hana/ada-amenities/index.php
@@ -0,0 +1,186 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once($path."/Library/Travaasa_Common.php");
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Travaasa Experiential Resorts - ADA Amenities</title>
+	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, ada, disability, disabilities, americans with disabilities act, compliance, amenities" />
+	<meta name="description" content="ADA Amenities list for Travaasa Hana, Maui Resort &amp; Spa" />
+	<?php 
+   include_once($path."/Library/Travaasa_Head.php");
+	?>
+<!-- start NAVIS Code -->
+<script type="text/javascript" src="http://www.navistechnologies.info/JavascriptPhoneNumber/js.aspx?account=15070&amp;jspass=ivcsp4gaxk4fbz8banxd&amp;dflt=<?php echo NAVIS_GLOBAL_HANA ?>"></script>
+<script type="text/javascript">ProcessNavisNCKeyword();</script>
+<script id="navis-fusion-loader"
+src="https://assets.navisperformance.com/NWRC/Fusion/navis-fusion-loader.js" data-accountid="15070" data-secret="ivcsp4gaxk4fbz8banxd"></script>
+<!-- end NAVIS Code -->
+</head>
+<body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MW4X3M"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MW4X3M');</script>
+<!-- End Google Tag Manager -->
+<?php 
+   include_once($path."/Library/Travaasa_Menu_Hana.php");
+?><section id="feature1_wrapper" class="subpage_bg"><img src="/assets/images/bg_hana_clouds.jpg" alt=""></section>
+<section id="content">
+<article id="main">
+  <section class="intro pad clearfix notheater">
+          <h2>Travaasa Hana ADA Amenities</h2>
+  </section>
+  <section class="grey pad group">
+          <div style="margin:10px 0 10px 15px;">
+            <h2 id="parkingamploadingzone">Parking &amp; Loading Zone</h2>
+
+<ul>
+<li>Adequate accessible parking signage at each accessible parking space.</li>
+
+<li>2 stalls in Main Parking lot</li>
+
+<li>1 at Hana Town Center</li>
+
+<li>1 at Hana Ranch Store</li>
+
+<li>1 at Ocean Bungalow Parking Lot</li>
+
+<li>1 at Housekeeping</li>
+</ul>
+
+<h2 id="buildingentrancesamplobby">Building Entrances &amp; Lobby</h2>
+
+<ul>
+<li>Port Cochere entrance is level and open space(no door ways)</li>
+
+<li>The Preserve main dining room entrance is level. The Preserve Bar entrance has a ramp entrance.</li>
+
+<li>Lobby entrance from Gallery is furnished with a ramp</li>
+</ul>
+
+<h2 id="interiorroutes">Interior Routes</h2>
+
+<ul>
+<li>Lobby corridors are at least 32”</li>
+
+<li>Doors widths are at least 32”</li>
+</ul>
+
+<h2 id="publiccommonuserestrooms">Public/Common Use Restrooms</h2>
+
+<ul>
+<li>The public restrooms in the main lobby have accessible stall</li>
+
+<li>Pool chair lift available for guests at wellness and garden pools  </li>
+
+<li>Pool chair lift available for guests at spa treatment pool </li>
+</ul>
+
+<h2 id="accessibleguestrooms">Accessible Guest Rooms</h2>
+
+<p>There are two (3) accessible guest rooms. (2) Garden View Room and (2) Ocean View Bungalows.</p>
+
+<h3 id="room312queenbeds">Room 31-2 Queen Beds</h3>
+
+<ul>
+<li>Door hardware (levers, pulls, etc) on all entry doors to accessible guest rooms and other passage doors within the rooms are usable with one hand, without tight grasping, pinching or twisting of the wrist;</li>
+
+<li>Accessible shower seat and toilet</li>
+
+<li>Bathroom doors greater than 32”</li>
+
+<li>19” raised toilet</li>
+
+<li>Grab Bars near the toilet</li>
+</ul>
+
+<h3 id="room352queenbeds">Room 35-2 Queen Beds</h3>
+
+<ul>
+<li>Door hardware (levers, pulls, etc) on all entry doors to accessible guest rooms and other passage doors within the rooms are usable with one hand, without tight grasping, pinching or twisting of the wrist;</li>
+
+<li>Accessible height bath tub with grab bars</li>
+
+<li>Adjustable shower head and grab bars</li>
+
+<li>Bathroom doors greater than 32”</li>
+
+<li>Grab Bars near the toilet</li>
+
+<li>19” raised toilet</li>
+
+<li>Roll under bar sink </li>
+
+<li>Accessible bar amenity shelves</li>
+
+<li>Wheelchair accessible clothing rod</li>
+</ul>
+
+<h3 id="room301">Room 301</h3>
+
+<ul>
+<li>Door hardware (levers, pulls, etc) on all entry doors to accessible guest rooms and other passage doors within the rooms are usable with one hand, without tight grasping, pinching or twisting of the wrist;</li>
+
+<li>Accessible height bath tub with grab bars</li>
+
+<li>Adjustable shower head and grab bars</li>
+
+<li>Bathroom doors greater than 32”</li>
+
+<li>Grab Bars near the toilet</li>
+
+<li>19” raised toilet</li>
+
+<li>Roll under bar sink </li>
+
+<li>Accessible bar amenity shelves</li>
+
+<li>48” curtain wand pull</li>
+</ul>
+
+<h3 id="room308">Room 308</h3>
+
+<ul>
+<li>Door hardware (levers, pulls, etc) on all entry doors to accessible guest rooms and other passage doors within the rooms are usable with one hand, without tight grasping, pinching or twisting of the wrist;</li>
+
+<li>Accessible height bath tub with grab bars</li>
+
+<li>Adjustable shower head and grab bars</li>
+
+<li>Bathroom doors greater than 32”</li>
+
+<li>Grab Bars near the toilet</li>
+
+<li>19” raised toilet</li>
+
+<li>Roll under bar sink </li>
+
+<li>Accessible bar amenity shelves</li>
+
+<li>48” curtain wand pull</li>
+</ul>
+          </div>
+  </section>
+</article>
+</section>
+<?php
+
+ 
+   include_once($path."/Library/Travaasa_Testimonials.php");
+   include_once($path."/Library/Travaasa_Footer_Hana.php");
+   include_once($path."/Library/Travaasa_Scripts.php");
+?><script type="text/javascript">
+$(function() {
+	$('a[data-target="flare"]').peFlareLightbox();
+	setLocation("Hana", "offproperty");
+});
+</script>
+</body>
+</html>
