f571545ce4386f9e4121acf8bbc842bed094c4b1
Tanner Powell <thetannerpowell@gmail.com>
2021-03-30 10:58:09 -0500

    About Us update

--- about-us/index.php
+++ about-us/index.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Travaasa Experiential Resorts | Destination Resorts | Hana Maui Hawaii</title>
+	<title>Travaasa Experiential Resorts | Green Tea MGMT</title>
 	<meta name="keywords" content="travaasa, travel, resorts, hana, hāna, hawaii, hawaiian islands, experiential, hawaii honeymoon, hawaii destination wedding" />
 	<meta name="description" content="Travaasa destination resorts in Hana, Maui, Hawaii offer a journey of discovery. All inclusive packages available." />
 	<?php
@@ -39,11 +39,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 We are more than just destination resorts with world-class spa services, cuisine inspired by local flavor and breathtaking vistas. Our hotels recapture the magic of travel and give you indelible memories. Travaasa Experiential Resorts embody the best of what happens when you travel: the journey and the discovery of a place, people, yourself and the stories that define your life. <strong>We invite you to join us on the journey.</strong></p>
   </section>
   <section class="pad grey group">
-    <div class="col span_1_of_2">
+    <!--<div class="col span_1_of_2">
       <a href="/hana"><img class="lazy" data-original="/assets/images/travaasa_hana.jpg" src="/assets/images/global/placeholder.gif" alt="A Magical Place Called Hana Travaasa Resort Hana, Maui"></a>
       <h2 class="title"><a href="/hana">A Magical Place Called Hana</a></h2>
-        <p class="description">Travaasa Hana, Maui is a destination resort unlike anywhere else in the world. Gracious in every sense of the word, from our staff to accommodations, dining, experiences, scenery and much more. We invite you to discover it for yourself. <a href="/hana">Travaasa Hana, Maui</a></p></div>
-  	<div class="col span_1_of_2">
+        <p class="description">Travaasa Hana, Maui is a destination resort unlike anywhere else in the world. Gracious in every sense of the word, from our staff to accommodations, dining, experiences, scenery and much more. We invite you to discover it for yourself. <a href="/hana">Travaasa Hana, Maui</a></p></div>-->
+  	<div class="col span_1_of_1">
     	<a href="/berkshire-county"><img class="lazy" data-original="/assets/images/berkshire-county.jpg" src="/assets/images/global/placeholder.gif" alt="Travaasa Berkshire County – Coming in 2019"></a>
     	<h2 class="title"><a href="/hana">A New Addition to the Family</a></h2>
         <p class="description">Elm Court is a gorgeous estate with truly unique character, significant history and a passionate community. Our team has spent years working on a plan that celebrates the beauty &amp; charm of the property, and we're ecstatic to begin making our vision a reality. We can't wait to host you and share the magic of travel at Travaasa Berkshire County. <a href="/berkshire-county">Travaasa Berkshire County</a></p>
